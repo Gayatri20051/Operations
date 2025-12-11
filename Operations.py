e56e2d7232bedd11af53616d54e09e5c9aa8ae86
Sai <01fe24ica044@kletech.ac.in>
2025-12-11 10:35:57 +0530

    new functions multipication and division

--- Operations.py
+++ Operations.py
@@ -1,6 +1,4 @@
 # operations.py
-# A simple program to perform addition and subtraction on two numbers
-
 # Read input from the user
 num1 = float(input("Enter first number: "))
 num2 = float(input("Enter second number: "))
@@ -8,9 +6,12 @@ num2 = float(input("Enter second number: "))
 # Performing addition and subtraction
 sum_result = num1 + num2
 diff_result = num1 - num2
-
+product_result = num1 * num2
+div_result = num1 / num2
 # Displaying the results
 print("Performing addition and subtraction")
 print("Results:")
 print("Addition (num1 + num2) =", sum_result)
 print("Subtraction (num1 - num2) =", diff_result)
+print("Multiplication(num1 * num2) =", product_result)
+print("Divison(num1 / num2) =", div_result)
